d71be27890c5b878a8223b7c9144c4980d16fdea
Breno L. Lopes <brernolopes@hotmail.com>
2022-05-15 09:55:32 -0300

    Adicionado os testes do header

--- cypress/integration/pages.spec.js/mainPage.spec.js
+++ cypress/integration/pages.spec.js/mainPage.spec.js
@@ -2,6 +2,18 @@ describe('Testando o testador', () => {
   beforeEach(() => {
     cy.visit('pages/mainPage.html');
   });
-  it('Simple test with true or false', () => {
-  })
+  it('Se possui um header com a cor correta', () => {
+    cy.get('header')
+    .should('have.css', 'backgroundColor', 'rgb(3, 107, 82)')
+  });
+  it('Possui um logo', () => {
+    cy.get('header')
+        .find('img')
+          .should('have.attr', 'src')
+  });
+  it('Possui a quantidade correta de links', () => {
+    cy.get('header')
+        .find('a')
+          .should('have.length', 8)
+  });
 })
