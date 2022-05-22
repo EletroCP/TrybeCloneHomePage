f00649fcfff2d57d814b9b8346e2ee773b1ba951
Breno L. Lopes <brernolopes@hotmail.com>
2022-05-22 14:38:36 -0300

    fix: Correção dos testes 03

--- cypress/integration/mainPage/03 - sectionTrainedPeople.spec.js
+++ cypress/integration/mainPage/03 - sectionTrainedPeople.spec.js
@@ -6,18 +6,14 @@ describe('Verifica se a section "trainedPeople" possui os recursos corretos', ()
   const trainedPeople = 'das pessoas formadas já estão trabalhando em até 3 meses após a formatura. Esse número é atualizado 90 dias após a conclusão de cada turma.';
 
   it('O componente possui um titulo', () => {
-    cy.get('body')
-        .find('section')
-          .should('have.class', 'trainedPeopleSection')
-            .find('h1')
-              .should('contain', '92%')
+    cy.get('section.trainedPeopleSection')
+        .find('h1')
+          .should('contain', '92%')
   });
 
   it('O componente possui o texto informativo', () => {
-    cy.ge('body')
-        .find('section')
-          .should('have.class', 'trainedPeopleSection')
-            .find('p')
-              .should('contain', trainedPeople)
+    cy.get('section.trainedPeopleSection')
+        .find('p')
+          .should('contain', trainedPeople)
   });
 });
