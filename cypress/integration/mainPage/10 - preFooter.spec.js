b4eb1c3b796061c6c24aea3e6b5a95164a6085db
Breno L. Lopes <brernolopes@hotmail.com>
2022-05-29 11:02:18 -0300

    fix: Refatorados os testes 10

--- cypress/integration/mainPage/10 - preFooter.spec.js
+++ cypress/integration/mainPage/10 - preFooter.spec.js
@@ -1,40 +1,34 @@
-descrive('Testa se o componente "preFooter" possui todos os elementos', () => {
+describe('Testa se o componente "preFooter" possui todos os elementos', () => {
   beforeEach(() => {
     cy.visit('pages/mainPage.html');
   });
 
-  const preFooterImage = 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61a6362ac5e4ff25cbd9aca8_footer.png';
+  const preFooterImage = '../src/preFooterImage.png';
   const preFooterTitle = 'A sua conquista é a nossa. A Trybe só ganha quando você começar a ganhar.';
   const preFooterText = 'Confiamos tanto no seu sucesso profissional que você não precisa pagar nada até estar trabalhando e com uma remuneração de, pelo menos, R$ 3.000,00.';
   it('O componente possui uma imagem', () => {
-    cy.get('body')
-        .find('section')
-          .should('have.class', 'preFooter')
-            .find('img')
-              .should('have.src', preFooterImage)
+    cy.get('section#prefooter')
+        .find('img')
+          .should('have.attr', 'src')
+            .should('contain', preFooterImage)
   });
 
   it('O componente possui um titulo', () => {
-    cy.get('body')
-        .find('section')
-          .should('have.class', 'preFooter')
-            .find('h1')
-              .should('have.value', preFooterTitle)
+    cy.get('section#prefooter')
+        .find('h1')
+          .should('contain', preFooterTitle)
   });
 
   it('O componente possui um texto descritivo', () => {
-    cy.get('body')
-        .find('section')
-          .should('have.class', 'preFooter')
-            .find('p')
-              .should('have.value', preFooterText)
+    cy.get('section#prefooter')
+        .find('p')
+          .should('contain', preFooterText)
   });
 
   it('O componente possui um link para o "modelo de sucesso compartilhado"', () => {
-    cy.get('body')
-        .find('section')
-          .should('have.class', 'preFooter')
-            .find('a')
-              .should('have.href', 'page/msc-modelo-de-sucesso-compartilhado.html')
+    cy.get('section#prefooter')
+        .find('a')
+          .should('have.attr', 'href')
+            .should('contain', './msc-modelo-de-sucesso-compartilhado.html')
   });
 });
